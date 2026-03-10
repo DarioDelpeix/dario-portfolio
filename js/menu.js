afe7ec42153ddaabae37760ea8248cbc3c660d91
Dario <dario.delpeix7@gmail.com>
2026-03-10 18:53:28 +0100

    cambio en el header

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 const menuButton = document.querySelector('.menu-toggle');
         const navMenu = document.getElementById('main-menu');
-
+        // al hacer click en el botón del menú, se alterna la clase 'is-open' en el menú de navegación y muestra o oculta el menú
         if (menuButton && navMenu) {
             menuButton.addEventListener('click', () => {
                 const isOpen = menuButton.getAttribute('aria-expanded') === 'true';
